495ca1c11ddaa9837d08c95bda77e17cf2aff0d2
Mohd-Samad-Siddiqui <samad.siddiqui47@gmail.com>
2024-01-24 17:04:58 +0000

    typo's correction

--- script.js
+++ script.js
@@ -16,76 +16,76 @@ let randomNumGenerator = parseInt(Math.random() * 100 + 1);
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   let guess = userInput.value;
-  
-  if(remainingGuess >= 0){
+
+  if (remainingGuess >= 0) {
     guessValidate(guess);
-  }else{
+  } else {
     endGame();
   }
-  
+
 })
 
-function guessValidate(guess){
-  if(guess < 1){
+function guessValidate(guess) {
+  if (guess < 1) {
     alert(`${guess} is less than 1, it has to be more than 1.`)
   }
-  else if(guess > 100){
-      alert(`${guess} is more than 100, it has to be less than 100.`)
-    }
-  else if(isNaN(guess)){
+  else if (guess > 100) {
+    alert(`${guess} is more than 100, it has to be less than 100.`)
+  }
+  else if (isNaN(guess)) {
     alert(`${guess} is invalid number.`)
   }
-  else{
+  else {
     remainingGuess = 10 - numberOfGuesses;
-    if(remainingGuess >= 0){
+    if (remainingGuess >= 0) {
       numberOfGuesses++;
       userInput.value = '';
       checkTheGuess(guess);
-    }else{
-      endGame(); 
+    } else {
+      endGame();
     }
   }
 }
 
-function checkTheGuess(guess){
-  if(remainingGuess === 9){
+function checkTheGuess(guess) {
+  if (remainingGuess === 9) {
     guessArray.innerHTML += guess;
-  }else{
+  } else {
     guessArray.innerHTML += `, ${guess}`;
   }
 
-  if(numberOfGuesses === 11 && (guess < randomNumGenerator || guess > randomNumGenerator)){
+  if (numberOfGuesses === 11 && (guess < randomNumGenerator || guess > randomNumGenerator)) {
     correctIncorrect.innerHTML = `You lost the game!!`
     remainingChance.innerHTML = `The right number was ${randomNumGenerator}`;
     lessMore.innerHTML = '';
     endGame();
   }
-  else if(guess < randomNumGenerator){
+  else if (guess < randomNumGenerator) {
     correctIncorrect.innerHTML = 'Incorrect!!!'
     remainingChance.innerHTML = `You have ${remainingGuess} attemps remaining`;
     lessMore.innerHTML = ": GO HIGH";
   }
-  else if(guess > randomNumGenerator){
+  else if (guess > randomNumGenerator) {
     correctIncorrect.innerHTML = 'Incorrect!!!'
     remainingChance.innerHTML = `You have ${remainingGuess} attemps remaining`;
     lessMore.innerHTML = ": GO LOW";
   }
-  else{
-    correctIncorrect.innerHTML = `Correct!!! You're absolutely right. The number was ${guess}`
+  else {
+    correctIncorrect.innerHTML = `You won the game!! The number was ${guess}`
     remainingChance.innerHTML = '';
     lessMore.innerHTML = '';
     endGame();
   }
 }
 
-function endGame(){
-  userInput.setAttribute('disabled', ''); 
+function endGame() {
+  userInput.setAttribute('disabled', '');
   playGame = false;
   newGame.style.display = 'block';
   startNewGame();
 }
 
-function startNewGame(){
+function startNewGame() {
   newGame.addEventListener('click', () => {
     playGame = true;
     userInput.removeAttribute('disabled');
